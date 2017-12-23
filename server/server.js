2dd3b43822941831cc0f9199d7b2260704d86f6e
jtodd819 <toddjam@oregonstate.edu>
2017-12-23 10:19:57 -0500

    styled mail form

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
+//setup express and body parser
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+//set port to specified environment variable or 3001
 app.set('port', process.env.PORT || 3001);
 
+//use a static build on production server
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client'));
 }
 
+//send mail using node mailer
+app.post('/sendmail', (req,res) => {
+	console.log(req.body);
+});
+
+//listen on specified port
 app.listen(app.get('port'), () => {
   console.log(`Server started at http://localhost:${app.get('port')}/`);
 });
--- src/components/MailForm.js
+++ src/components/MailForm.js
@@ -1,36 +1,78 @@
 import React, { Component } from 'react';
+import { FormControl, FormGroup, ControlLabel } from 'react-bootstrap';
+import '../style/MailForm.css';
 
 //Form for Sending Mail to my Address
 class MailForm extends Component{
+
+	//messages are composed of address,subject, and message
 	constructor(props){
 		super(props);
-		this.state = {address:'', subject:'', message:'', sent: false};
+		this.state = {address:'', subject:'', message:''};
 		this.handleTextInput = this.handleTextInput.bind(this);
 		this.handleSend = this.handleSend.bind(this);
+		this.getValidationState = this.getValidationState.bind(this);
 	}
+
+	//update state of each input based on user input
 	handleTextInput(event){
 		const value = event.target.value;
 		const name = event.target.name;
 		this.setState({[name]: value});
 	}
+
+	//If the address subject and message are specified send mail using nodemail posted to the express sendmail route
 	handleSend(event){
-		alert('Message Sent.');
+		if(this.state.address === '' || this.state.subject === '' || this.state.message === ''){
+			alert('Please include a valid address, subject, and message to send a contact email.');
+		}else{
+			fetch('/sendmail', {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					address: this.state.address,
+					subject: this.state.subject,
+					message: this.state.message,
+				}),
+			});
+		}
 		event.preventDefault();
 	}
+	
+	//Return the validity of an input field based on its length
+	getValidationState(length){
+		console.log(length);
+		if(length > 0){
+			return 'success';
+		}
+		return 'error';
+	}
+
+	//create form for submitting email
 	render(){
 		return(
-			<form onSubmit={this.handleSend}>
-				<label>Address:
-					<input name='address' type='text' value={this.state.address} onChange={this.handleTextInput}/>
-				</label>
-				<label>Subject:
-					<input name='subject' type='text' value={this.state.subject} onChange={this.handleTextInput}/>
-				</label>
-				<br/>
-				<label>Message:
-					<input name='message' type='text' value={this.state.message} onChange={this.handleTextInput}/>
-				</label>
-				<input type='submit' value='Send'/>
+			<form className='mailForm' onSubmit={this.handleSend}>
+				<FormGroup validationState={this.getValidationState(this.state.address.length)}>
+					<ControlLabel>Address:</ControlLabel><br/>
+					<FormControl name='address' type='text' value={this.state.address} 
+					onChange={this.handleTextInput}/><br/>
+				</FormGroup>
+				<FormGroup validationState={this.getValidationState(this.state.subject.length)}>
+					<ControlLabel>Subject:</ControlLabel><br/>
+					<FormControl name='subject' type='text' value={this.state.subject} 
+					onChange={this.handleTextInput}/><br/>
+				</FormGroup>
+				<FormGroup validationState={this.getValidationState(this.state.message.length)}>
+					<ControlLabel>Message:</ControlLabel><br/>
+					<FormControl name='message' type='text' value={this.state.message}
+					componentClass='textarea' onChange={this.handleTextInput}/><br/>
+				</FormGroup>
+				<div style={{textAlign: 'center'}}>
+					<input type='submit' value='Send'/>
+				</div>
 			</form>
 		);
 	}
