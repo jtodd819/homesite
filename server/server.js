841eaca4430170e7da7d873a95092ac60bba0c19
Jake Todd <jaketodd819@gmail.com>
2017-12-27 16:32:32 +0000

    nodemailer

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const nodemailer = require('nodemailer');
+const config = JSON.parse(fs.readFileSync('server/config.json'));
+
 
 //setup express and body parser
 const app = express();
@@ -14,9 +19,39 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client'));
 }
 
+//setup nodemailer transport
+let transporter = nodemailer.createTransport({
+	host: 'smtp.gmail.com',	
+	port: 465,
+	secure: true,
+	auth: {
+		type: 'OAuth2',
+		clientId: config.id,
+		clientSecret: config.secret
+	},
+});
+
+
 //send mail using node mailer
 app.post('/sendmail', (req,res) => {
-	console.log(req.body);
+	transporter.sendMail({
+		from: config.sender,
+		to: config.receiver,
+		subject: `<${req.body.address}> ${req.body.subject}`,
+		text: req.body.message,
+		auth: {
+			user: config.sender,
+			refreshToken: config.refresh,
+			accessToken: config.access,
+			expires: 3500
+		}
+	}, (err,res) => {
+		if(err){
+			console.log(err);
+		}else{
+			console.log('message sent');
+		}
+	});
 });
 
 //listen on specified port
