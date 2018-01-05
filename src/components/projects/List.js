77f58f545b5937566447dcb135a9f9c4f68a8064
Jake Todd <jaketodd819@gmail.com>
2018-01-05 15:33:49 +0000

    added small shell project

--- src/components/projects/List.js
+++ src/components/projects/List.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import plannerPic from '../../media/workoutPlanner.png';
+import smallShellPic from '../../media/smallShell.png';
 import { Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
@@ -12,12 +13,18 @@ class List extends Component{
 					<Image src={plannerPic} responsive thumbnail/>
 				</div>
 				<Link to='/projects/workoutplanner'>Workout Planner </Link>
-				<a href='https://github.com/jtodd819/homesite/tree/master/src/components/projects/workoutplanner'>
+				<a href='https://github.com/jtodd819/homesite/tree/master/src/components/projects/workoutplanner'> 
 				<i className='fa fa-github'/></a><br/>
 				<p className='text-primary'>A workout planner for calculating set and rep counts for inputted exercises <br/>
 				based on selected one rep maximum weights or maximum rep counts if the exercise is weightless. <br/>
 				Backend saves, queries, edits, and deletes exercises on a MongoDB cluster using Mongoose ODM and Express. <br/>
-				Front end components built with ReactJS with styling from React Bootstrap.</p>
+				Front end components built with ReactJS with styling from React Bootstrap.</p><br/>
+				<div style={{width: 700, height: 'auto'}}>
+					<Image src={smallShellPic} responsive thumbnail/>
+				</div>
+				<a href='https://github.com/jtodd819/smallshell'>Small Shell <i className='fa fa-github'/></a><br/>
+				<p className='text-primary'>A small shell program written in C to execute commands using the 
+				fork(), exec(), and waitpid() functions.</p><br/>
 			</div>
 		);
 	}
