3d6599ff3afef467a7a7af4b07376720c256f1b2
Jake Todd <jaketodd819@gmail.com>
2018-02-21 20:20:40 +0000

    added my recipes project

--- src/components/projects/List.js
+++ src/components/projects/List.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import plannerPic from '../../media/workoutPlanner.png';
 import smallShellPic from '../../media/smallShell.png';
+import recipesPic from '../../media/recipes.png';
 import { Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
@@ -20,6 +21,16 @@ class List extends Component{
 				Backend saves, queries, edits, and deletes exercises on a MongoDB cluster using Mongoose ODM and Express with
 				localStorage created users. <br/>
 				Front end components built with ReactJS with styling from React Bootstrap.</p><br/>
+				<div style={{width: 700, height: 'auto'}}>
+					<Image src={recipesPic} responsive thumbnail/>
+				</div>
+				<a href='https://myrecipes-194614.appspot.com/'>My Recipes</a>
+				<a href='https://github.com/jtodd819/Recipes'> 
+				<i className='fa fa-github'/></a><br/>
+				<p className='text-primary'>Python 2.7 Google App Engine recipe tracker connected to a Rest API<br/>
+				that submits recipes and ingredients to a Google Cloud Datastore database instance. Uses OAuth2 with Google People API<br/>
+				to access basic account information to view/create/edit/delete recipes for an authenticated user.<br/>
+				API routes listed in Github Readme.</p><br/>
 				<div style={{width: 700, height: 'auto'}}>
 					<Image src={smallShellPic} responsive thumbnail/>
 				</div>
