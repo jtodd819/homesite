46cc0b4cee31f2b495f37d10ddc47c6fb0e57cbb
Jake Todd <jaketodd819@gmail.com>
2020-09-09 08:14:17 -0500

    update mail form

--- src/components/MailForm.js
+++ src/components/MailForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { FormControl, FormGroup, FormLabel } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
+import  { Formik } from 'formik'
+import * as Yup from 'yup';
 import '../style/MailForm.css';
 
 //Form for Sending Mail to my Address
@@ -9,84 +11,102 @@ class MailForm extends Component{
 	constructor(props){
 		super(props);
 		this.state = { address:'', subject:'', message:'' };
-		this.handleTextInput = this.handleTextInput.bind(this);
 		this.handleSend = this.handleSend.bind(this);
-		this.getLengthValidation = this.getLengthValidation.bind(this);
-		this.getAddressValidation = this.getAddressValidation.bind(this);
+		this.schema = this.getFormSchema.bind(this);
 	}
 
-	//update state of each input based on user input
-	handleTextInput(event){
-		const value = event.target.value;
-		const name = event.target.name;
-		this.setState({[name]: value});
+	/**
+	 * Sets up Yup form schema for validation
+	 */
+	getFormSchema() {
+		return Yup.object().shape({
+			from: Yup.string()
+				.email('Invalid email')
+				.required('Required'),
+			subject: Yup.string()
+				.max(50, 'Too Long!')
+				.required('Required'),
+			body: Yup.string()
+				.required('Required')
+		});
 	}
 
 	//If the address subject and message are specified send mail using nodemail posted to the express sendmail route
 	handleSend(event){
-		//Display error alert if address not in correct form or subject/message left blank
-		if(!/[^\s@]+@[^\s@]+\.[^\s@]+/.test(this.state.address) || this.state.subject === '' || this.state.message === ''){
-			alert('Please ensure you have included a subject, message, and valid email address in the contact form to send an email.');
-		}else{
-			fetch('/sendmail', {
-				method: 'POST',
-				headers: {
-					Accept: 'application/json',
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					address: this.state.address,
-					subject: this.state.subject,
-					message: this.state.message,
-				}),
-			}).then(alert('Message Sent!'));
-		}
+		fetch('/sendmail', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				address: this.state.address,
+				subject: this.state.subject,
+				message: this.state.message,
+			}),
+		}).then(alert('Message Sent!'));
 		event.preventDefault();
-	}
-
-	//Return the validity of an input field based on its length
-	getLengthValidation(length){
-		if(length > 0){
-			return 'success';
-		}
-		return 'error';
-	}
-
-	//validate email address based on regex pattern
-	getAddressValidation(address){
-		if(/[^\s@]+@[^\s@]+\.[^\s@]+/.test(address)){
-			return 'success';
-		}
-		return 'error';
 	}
 
 	//create form for submitting email
 	render(){
 		return(
-			<div>
-				<p className='text-primary' style={{textAlign: 'center'}}>To send me an email, please submit a message 
-				with your email address and a subject in the form below.</p>
-				<form className='mailForm' onSubmit={this.handleSend}>
-					<FormGroup validationState={this.getAddressValidation(this.state.address)}>
-						<FormLabel>Address:</FormLabel><br/>
-						<FormControl name='address' type='text' value={this.state.address} 
-						onChange={this.handleTextInput}/><br/>
-					</FormGroup>
-					<FormGroup validationState={this.getLengthValidation(this.state.subject.length)}>
-						<FormLabel>Subject:</FormLabel><br/>
-						<FormControl name='subject' type='text' value={this.state.subject} 
-						onChange={this.handleTextInput}/><br/>
-					</FormGroup>
-					<FormGroup validationState={this.getLengthValidation(this.state.message.length)}>
-						<FormLabel>Message:</FormLabel><br/>
-						<FormControl name='message' type='text' value={this.state.message}
-						componentClass='textarea' onChange={this.handleTextInput}/><br/>
-					</FormGroup>
-					<div style={{textAlign: 'center'}}>
-						<input type='submit' value='Send'/>
-					</div>
-				</form>
-			</div>
+			<Formik
+				validationSchema={this.schema}
+				onSubmit={console.log}
+				initialValues={{
+					from: "",
+					subject: "",
+					body: ""
+				}}
+			>
+				{({
+					values,
+					errors,
+					touched,
+					handleChange,
+					handleSubmit,
+					isValid,
+				}) => (
+					<Form noValidate onSubmit={handleSubmit}>
+						<Form.Group controlId="validationFormikFrom">
+							<Form.Label>From:</Form.Label>
+							<Form.Control
+								name="from"
+								type="text"
+								value={values.from}
+								onChange={handleChange}
+								isValid={touched.from && !errors.from}/>
+							<Form.Control.Feedback>Nice!</Form.Control.Feedback>
+						</Form.Group>
+						<Form.Group>
+							<Form.Label>To:</Form.Label>
+							<Form.Control plaintext readOnly defaultValue="me@james-todd.net"/>
+						</Form.Group>
+						<Form.Group controlId="validationFormikSubject">
+							<Form.Label>Subject:</Form.Label>
+							<Form.Control
+								name="subject"
+								type="text"
+								value={values.subject}
+								onChange={handleChange}
+								isValid={touched.subject && !errors.subject}/>
+							<Form.Control.Feedback>Nice!</Form.Control.Feedback>
+						</Form.Group>
+						<Form.Group controlId="validationFormikBody">
+							<Form.Label>Body:</Form.Label>
+							<Form.Control
+								name="body"
+								type="text"
+								value={values.body}
+								onChange={handleChange}
+								isValid={touched.body && !errors.body}/>
+							<Form.Control.Feedback>Nice!</Form.Control.Feedback>
+						</Form.Group>
+						<Button type="submit"></Button>
+					</Form>
+				)}
+			</Formik>
 		);
 	}
 }
