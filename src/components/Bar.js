a525121763209e9ba33bca212c38ccf3ff6a4830
Jake Todd <jaketodd819@gmail.com>
2020-09-11 20:59:47 -0500

    Update Contact page

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -9,8 +9,9 @@ class Bar extends Component{
 			<Navbar className="bar">
 				<Navbar.Brand href="/home">Home</Navbar.Brand>
 				<Nav.Link href="/projects">Projects</Nav.Link>
-				<Nav.Link href="/contact">Contact</Nav.Link>
-				<Nav.Link href="https://github.com/jtodd819"><i className='fa fa-github icon'/></Nav.Link>
+				<Nav.Link href="/contact"><i className='fa fa-envelope-square icon'/></Nav.Link>
+				<Nav.Link href="https://linkedin.com/in/james-todd-827069144"><i className='fa fa-linkedin-square icon'/></Nav.Link>
+				<Nav.Link href="https://github.com/jtodd819"><i className='fa fa-github-square icon'/></Nav.Link>
 			</Navbar>
 		);
 	}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,28 @@
 import React, {Component} from 'react';
-import { Card, Container, Row, Col, Image } from 'react-bootstrap';
+import { Card, Container, Row, Col } from 'react-bootstrap';
 import '../style/Home.css';
 import me from '../media/pic.JPG';
-import resume from '../media/Resume.JPG'
+import Resume from './Resume';
 
 class Home extends Component{
 	render(){
 		return(
 			<Container fluid>
 				<Row>
-					<Col md={{offset: 1}}>
+					<Col>
 						<Card border="dark">
 							<Card.Img variant="top" src={me}/>
 							<Card.Body>
 								<Card.Title>Welcome</Card.Title>
 								<Card.Text>
 									My name is James Todd, and this is my personal website. I am a programmer specializing in Fullstack web development. 
-									For samples of my work, visit the Projects tab. For business inquiries visit the Contact tab.
+									For samples of my work, visit the Projects tab. For business inquiries, click on the mail or other social media icons.
 								</Card.Text>
 							</Card.Body>
 						</Card>
 					</Col>
-					<Col md={{offset: 1}}>
-						<Image fluid style={{border: "1px solid black", height: '800px'}} src={resume}/>
+					<Col>
+						<Resume></Resume>
 					</Col>
 				</Row>
 			</Container>
